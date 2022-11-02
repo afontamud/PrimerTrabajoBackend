068aa4ec13056081197f8a9d56caf0daea6bfa1e
afontamud <arielfontaph@gmail.com>
2022-11-02 20:10:02 -0300

    agregado

--- primerdesafio.js
+++ primerdesafio.js
@@ -1,27 +1,52 @@
-class usuario {
+class Usuario {
     constructor (nombre, apellido, libros, mascotas) {
         this.nombre = nombre
         this.apellido = apellido
-        this.libros = libros
-        this.mascotas = mascotas
+        this.mascotas = mascotas [Raza, NombreMascota]
+        Object.libro[
+            this.Obra = undead
+
+        ]
     }
 
+
     getfullname(){ 
-    
+        this.nombre
+        this.apellido
     }
     
     
     addMascota(string)
     
-    countMascotas() {
+ 
+    countMascotas(mascotas) {
+        this.mascotas = mascotas
+        this.cuentaLocal = 0
+    }
+    static cuentaGlobal = 0
+
+    contar(){
+        this.cuentaLocal++
+        Contador.cuentaGlobal++
         return Contador.countMascotas;
     }
 }
 
 
-const user1 = {nombre:'leo', apellido: 'Sotero', libros, mascotas: perro}
+const User1 = new Usuario ('leo', 'Sotero', libros, perro)
+User1 = {}
+User1.nombre = "Leo"
+User1.apellido = "Sotero"
 
-const user2 = {nombre: 'Laura', apellido: 'Lovo', libros, mascotas: gato}
+mascotas.Contar()
+mascotas.contar()
 
-console.log(user1)
-console.log(user2)
+
+
+const User2 = new Usuario ('Laura', 'Lovo', libros, gato)
+User1.nombre = "Laura"
+User1.apellido = "Lovo"
+User1.mascotas = gato 
+
+console.log(User1)
+console.log(User2)
